36c1102bc7b628b35c2b1441a2be95f41b1be2b0
Shruthi Bidarikere <57198354+shrunagraj14@users.noreply.github.com>
2021-03-30 13:59:29 -0400

    about views

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse 
 
-def index(request): 
+def home(request): 
 
-    return HttpResponse("Hello World!! ") 
+    return HttpResponse("Hello, Welcome to the Home Page!! ") 
+
+def about(request):
+    return HttpResponse("This is the About us page!!")
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -19,5 +19,6 @@ from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.home, name ='home'),
+    path('about/', views.about),
 ]
